54d44c3d54df459a867418058cd6e269ca392b49
danielamuniz367 <munizd@oregonstate.edu>
2018-09-23 23:24:52 -0500

    Very first CS 165 programming assignment

---
+++ animal.cpp
@@ -0,0 +1,23 @@
+/********************************************************************
+** Author: Daniela Muniz 
+** Date: January 9, 2018
+** Description: Assignment 1.a 
+*********************************************************************/
+
+#include <iostream>
+#include <string>
+
+// A simple example program
+int main()
+{
+    std::string faveAnimal;
+    std::cout << "Please enter your favorite animal." << std::endl;
+    std::cin >> faveAnimal;
+    std::cout << "Your favorite animal is the " << faveAnimal;
+    std::cout << "." << std::endl;
+
+    return 0;
+
+}
+
+
